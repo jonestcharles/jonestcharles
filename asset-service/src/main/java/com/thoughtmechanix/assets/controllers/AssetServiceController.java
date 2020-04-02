1d88fef5164cfc58e688589f487bc0209e3c17c6
Jones <jonest59@medtronic.com>
2020-04-02 12:55:30 -0500

    final

---
+++ asset-service/src/main/java/com/thoughtmechanix/assets/controllers/AssetServiceController.java
@@ -0,0 +1,82 @@
+package com.thoughtmechanix.assets.controllers;
+
+import com.thoughtmechanix.assets.config.ServiceConfig;
+import com.thoughtmechanix.assets.model.Asset;
+import com.thoughtmechanix.assets.services.AssetService;
+import com.thoughtmechanix.assets.utils.UserContextHolder;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.http.ResponseEntity;
+
+import java.util.List;
+
+@RestController
+@RequestMapping(value="/v1/organizations/{organizationId}/assets")
+public class AssetServiceController {
+    private static final Logger logger = LoggerFactory.getLogger(AssetServiceController.class);
+
+    @Autowired
+    private AssetService assetService;
+
+    @Autowired
+    private ServiceConfig serviceConfig;
+
+    /**
+     * GET all assets from an Organization
+     * @param organizationId the Organization to get Assets of
+     * @return ResponseEntity of Assets as JSON
+     */
+    @RequestMapping(value="/",method = RequestMethod.GET)
+    public List<Asset> getAssets(@PathVariable("organizationId") String organizationId) {
+        logger.debug("AssetServiceController Correlation id: {}", UserContextHolder.getContext().getCorrelationId());
+        return assetService.getAssetsByOrg(organizationId);
+    }
+
+    /**
+     * GET an Asset from an Organization by id
+     * @param organizationId
+     * @param assetId
+     * @return ResponseEntity with Asset as JSON
+     */
+    @GetMapping(value="/{assetId}")
+    public ResponseEntity<?> getAsset(@PathVariable("organizationId") String organizationId,
+                                      @PathVariable("assetId") String assetId) {
+
+        return new ResponseEntity<>(assetService.getAsset(organizationId, assetId, ""), HttpStatus.OK);
+    }
+
+    /**
+     * PUT mapping that updates an Asset with newAsset
+     * @param newAsset
+     * @param assetId
+     * @return ResponseEntity
+     */
+    @PutMapping(value="/{assetId}")
+    public ResponseEntity<?> updateAsset(@RequestBody Asset newAsset, @PathVariable("assetId") String assetId) {
+        return new ResponseEntity<>(assetService.updateAsset(newAsset), HttpStatus.OK);
+    }
+
+    /**
+     * POST method creating a new Asset with given params
+     * @param asset
+     * @return ResponseEntity confirming POST
+     */
+    @PostMapping(value="/")
+    public ResponseEntity<?> saveAsset(@RequestBody Asset asset) {
+        return new ResponseEntity<>(assetService.saveAsset(asset), HttpStatus.CREATED);
+    }
+
+    /**
+     * DELETE method that deletes an Asset by ID
+     * @param assetId
+     * @return ResponseEntity
+     */
+    @DeleteMapping(value="/{assetId}")
+    public ResponseEntity<?> deleteAsset( @PathVariable("assetId") String assetId) {
+        assetService.deleteAsset(assetId);
+        return new ResponseEntity<>(HttpStatus.NO_CONTENT);
+    }
+}
